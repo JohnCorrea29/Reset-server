af6df3d60634e2205e43f1c6fbace1bec1ececa6
John <jecl2912@gmail.com>
2021-06-04 09:18:02 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // ===============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ===============================
+// Entorno
+// ===============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===============================
+// BD
+// ===============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Gin:I6UCvR3XuQEr8cl9@cluster0.bci2p.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
